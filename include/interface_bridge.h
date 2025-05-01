9b144addd4a73067248a71c99fc51d1dbaee2b63
Yuval Kogan <Yuvalkogan123@gmail.com>
2025-05-01 11:16:33 +0300

    Finish bridge class

--- include/interface_bridge.h
+++ include/interface_bridge.h
@@ -8,13 +8,19 @@
 class InterfaceBridge 
 {
 public:
-    InterfaceBridge(std::vector<std::string>& interfaces);
+    InterfaceBridge(const std::string& bridge_name, const std::vector<std::string>& interfaces);
     ~InterfaceBridge();
     
     void start();
     void stop();
+    void add_interface(const std::string& interface);
 
 private:
+    void _create_bridge();
+    void _delete_bridge();
+    void _set_interface_state(const std::string& interface, bool state_up);
+ 
+    std::string _bridge_name;
     std::vector<std::string> _interfaces;
     bool _is_active;
 };
--- src/interface_bridge.cpp
+++ src/interface_bridge.cpp
@@ -1,18 +1,58 @@
 #include "interface_bridge.h"
+#include <stdlib.h>
 
-InterfaceBridge::InterfaceBridge(std::vector<std::string>& interfaces)
+InterfaceBridge::InterfaceBridge(const std::string& bridge_name, const std::vector<std::string>& interfaces)
+    : _bridge_name(bridge_name), _interfaces(), _is_active(false)
 {
-    this->_interfaces = interfaces;
+    this->_create_bridge();
+    for (const std::string& interface : interfaces)
+    {
+        this->add_interface(interface);
+    }
 }
 
 InterfaceBridge::~InterfaceBridge()
 {
+    this->_delete_bridge();
 }
 
 void InterfaceBridge::start()
 {
+    this->_set_interface_state(_bridge_name, true);
+    this->_is_active = true;
 }
 
 void InterfaceBridge::stop()
 {
+    this->_set_interface_state(_bridge_name, false);
+    this->_is_active = false;
+}
+
+void InterfaceBridge::_create_bridge()
+{
+    // TODO handle errors
+
+    std::string command = "sudo ip link add name " + this->_bridge_name + " type bridge";
+    system(command.c_str());
+}
+
+void InterfaceBridge::add_interface(const std::string& interface)
+{
+    std::string command = "sudo ip link set " + interface + " master " + this->_bridge_name;
+    system(command.c_str());
+
+    this->_interfaces.push_back(interface);
+}
+
+void InterfaceBridge::_delete_bridge()
+{
+    std::string command = "sudo ip link delete " + this->_bridge_name;
+    system(command.c_str());
+}
+
+void InterfaceBridge::_set_interface_state(const std::string& interface, bool state_up)
+{
+    std::string command = "sudo ip link set " + interface + " ";
+    command += state_up ? "up" : "down";
+    system(command.c_str());
 }
